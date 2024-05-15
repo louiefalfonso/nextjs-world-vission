6a9e62d22c098725a19710ffe3c1fd0b6ea7603c
Louie Alfonso <lfriasalfonso@gmail.com>
2024-05-15 21:26:25 +0100

    World Vission Charity Project

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+import Blog from "@/components/blog";
+import Wrapper from "@/layout/Wrapper";
+
+export const metadata = {
+   title: "World Vission - Charity & Donation React Next JS Project",
+};
+const index = () => {
+   return (
+      <Wrapper>
+         <Blog/>
+      </Wrapper>
+   )
+}
+
+export default index
---
+++ src/components/blog/index.tsx
@@ -0,0 +1,19 @@
+import Breadcrumb from "@/components/common/Breadcrumb"
+import Footer from "@/layout/Footer"
+import Header from "@/layout/Header"
+import BlogArea from "./BlogArea"
+
+const Blog = () => {
+   return (
+      <>
+         <Header style_1={false} style_2={false} />
+         <main>
+            <Breadcrumb page_title="Blog Post" page_list="Blog" style={false} />
+            <BlogArea/>
+         </main>
+         <Footer />
+      </>
+   )
+}
+
+export default Blog;
