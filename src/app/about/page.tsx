9dccf4314ad98c6050dcf3a724e08a447f7ad2c5
Louie Alfonso <lfriasalfonso@gmail.com>
2024-05-16 22:09:50 +0100

    World Vission Charity Project

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import AboutUs from "@/components/inner-pages/about";
+import Wrapper from "@/layout/Wrapper";
+
+export const metadata = {
+   title: "World Vission - Charity & Donation React Next JS Project",
+};
+const index = () => {
+   return (
+      <Wrapper>
+         <AboutUs/>
+      </Wrapper>
+   )
+}
+
+export default index
---
+++ src/components/inner-pages/about/index.tsx
@@ -0,0 +1,29 @@
+import Brand from "@/components/common/Brand"
+import Breadcrumb from "@/components/common/Breadcrumb"
+import About from "@/components/home/About"
+import CtaArea from "@/components/home/CtaArea"
+import FAQ from "@/components/home/FAQ"
+import Features from "@/components/home/Features"
+import Testimonial from "@/components/home/Testimonial"
+import Footer from "@/layout/Footer"
+import Header from "@/layout/Header"
+
+const AboutUs = () => {
+   return (
+      <>
+         <Header style_1={false} style_2={true} />
+         <main>
+            <Breadcrumb page_title="About Us" page_list="About" style={false} />
+            <Brand/>
+            <About/>
+            <Features/>
+            <Testimonial />
+            <CtaArea/>
+            <FAQ/>
+         </main>
+         <Footer />
+      </>
+   )
+}
+
+export default AboutUs;
